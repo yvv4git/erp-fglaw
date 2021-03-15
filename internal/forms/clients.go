58c3afc35072352b250316be83490b8e2d8a5c1c
Владимир Елисеев <ve@kvado.ru>
2021-03-15 19:47:28 +0300

    Form Clients - methods and tests.

--- internal/forms/clients.go
+++ internal/forms/clients.go
@@ -1,5 +1,10 @@
 package forms
 
+import (
+	"github.com/yvv4git/erp-fglaw/internal/domain"
+	"gorm.io/gorm"
+)
+
 // Clients ...
 type Clients struct {
 	Pagination   Pagination
@@ -9,4 +14,64 @@ type Clients struct {
 	CuitCustomer string `valid:"length(0|20)" json:"cuit"`
 	ClientPhone  string `valid:"length(0|20)" json:"phone"`
 	ClientTypeID int64  `valid:"type(int64)" json:"typeid"`
+}
+
+// ReadPage is used for find clients entities.
+func (f *Clients) ReadPage(db *gorm.DB) (result []domain.Clients, err error) {
+	// Fill from form.
+	var clients domain.Clients
+	clients.ID = int(f.ID)
+	clients.Number = int(f.Number)
+	clients.Address = f.Address
+	clients.CuitCustomer = f.CuitCustomer
+	clients.ClientPhone = f.ClientPhone
+	clients.ClientTypeID = int(f.ClientTypeID)
+
+	// Find in data storage.
+	err = db.Where(&clients).
+		Offset(f.Pagination.Offset()).
+		Limit(int(f.Pagination.Limit)).
+		Find(&result).Error
+
+	return result, err
+}
+
+// Create is used for create clients entity in data storage.
+func (f *Clients) Create(db *gorm.DB) (err error) {
+	// Fill from form.
+	var clients domain.Clients
+	clients.ID = int(f.ID)
+	clients.Number = int(f.Number)
+	clients.Address = f.Address
+	clients.CuitCustomer = f.CuitCustomer
+	clients.ClientPhone = f.ClientPhone
+	clients.ClientTypeID = int(f.ClientTypeID)
+
+	return db.Create(&clients).Error
+}
+
+// Update is used for update entity in data storage.
+func (f *Clients) Update(db *gorm.DB) (err error) {
+	var clients domain.Clients
+
+	// Find record in db.
+	clients.ID = int(f.ID)
+	db.First(&clients)
+
+	// Fill from form.
+	clients.ID = int(f.ID)
+	clients.Number = int(f.Number)
+	clients.Address = f.Address
+	clients.CuitCustomer = f.CuitCustomer
+	clients.ClientPhone = f.ClientPhone
+	clients.ClientTypeID = int(f.ClientTypeID)
+
+	return db.Save(&clients).Error
+}
+
+// Delete is used for delete clients entity from data storage.
+func (f *Clients) Delete(db *gorm.DB) (err error) {
+	var clients domain.Clients
+	clients.ID = int(f.ID)
+	return db.Delete(&clients).Error
 }
--- internal/forms/clients_test.go
+++ internal/forms/clients_test.go
@@ -4,7 +4,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/yvv4git/erp-fglaw/internal/domain"
 	"github.com/yvv4git/erp-fglaw/internal/forms"
+	"github.com/yvv4git/erp-fglaw/tests"
+	"gorm.io/gorm"
 )
 
 func TestClients_Validate(t *testing.T) {
@@ -73,6 +76,189 @@ func TestClients_Validate(t *testing.T) {
 				assert.NotEmpty(t, err, tc.description)
 			} else {
 				assert.Empty(t, err, tc.description)
+			}
+		})
+	}
+}
+
+func TestClientsForm_ReadPage(t *testing.T) {
+	testCases := []struct {
+		name        string
+		form        forms.Clients
+		check       func(db *gorm.DB, result []domain.Clients)
+		wantErr     bool
+		description string
+	}{
+		{
+			name: "Find first page.",
+			form: forms.Clients{
+				Pagination: forms.Pagination{
+					Page:  0,
+					Limit: 10,
+				},
+			},
+			check: func(db *gorm.DB, result []domain.Clients) {
+				assert.Equal(t, 3, len(result))
+			},
+			wantErr:     false,
+			description: "Find clients entities for first page.",
+		},
+		{
+			name: "Find second page.",
+			form: forms.Clients{
+				Pagination: forms.Pagination{
+					Page:  1,
+					Limit: 10,
+				},
+			},
+			check: func(db *gorm.DB, result []domain.Clients) {
+				assert.Equal(t, 0, len(result))
+			},
+			wantErr:     false,
+			description: "Find clients entities for second page. No found.",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tests.PrepareTestDatabase()
+
+			result, err := tc.form.ReadPage(db)
+			if tc.wantErr {
+				assert.NotNil(t, err)
+			} else {
+				assert.Nil(t, err)
+				tc.check(db, result)
+			}
+		})
+	}
+}
+
+func TestClientsForm_Create(t *testing.T) {
+	testCases := []struct {
+		name        string
+		form        forms.Clients
+		check       func(db *gorm.DB)
+		wantErr     bool
+		description string
+	}{
+		{
+			name: "Create clients entity.",
+			form: forms.Clients{
+				Number:       4,
+				Address:      "2790 Green Street San-Francisco, CA 94123",
+				CuitCustomer: "Some unknown string",
+				ClientPhone:  "+17133373300",
+				ClientTypeID: 1,
+			},
+			check: func(db *gorm.DB) {
+				var clients domain.Clients
+				clients.Number = 4
+				err := db.
+					Joins("JOIN client_types on client_types.id = clients.client_type_id").
+					Preload("ClientType").
+					Where("number = ?", 4).
+					First(&clients).Error
+				assert.Nil(t, err, "Find new client")
+				assert.Equal(t, "Orba Social", clients.ClientType.ClientType, "Relation join table")
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tests.PrepareTestDatabase()
+
+			err := tc.form.Create(db)
+			if tc.wantErr {
+				assert.NotNil(t, err, tc.description)
+			} else {
+				assert.Nil(t, err, tc.description)
+				tc.check(db)
+			}
+		})
+	}
+}
+
+func TestClientsForm_Update(t *testing.T) {
+	testCases := []struct {
+		name        string
+		form        forms.Clients
+		check       func(db *gorm.DB)
+		wantErr     bool
+		description string
+	}{
+		{
+			name: "Update first client",
+			form: forms.Clients{
+				ID:           1,
+				Number:       1,
+				Address:      "1-2790 Green Street San-Francisco, CA 94123",
+				CuitCustomer: "Some unknown string",
+				ClientPhone:  "+17133373301",
+				ClientTypeID: 1,
+			},
+			wantErr: false,
+			check: func(db *gorm.DB) {
+				var clients domain.Clients
+				err := db.First(&clients, 1).Error
+				assert.Nil(t, err)
+				assert.Equal(t, "1-2790 Green Street San-Francisco, CA 94123", clients.Address)
+			},
+			description: "Update first client and then check it",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tests.PrepareTestDatabase()
+
+			err := tc.form.Update(db)
+			if tc.wantErr {
+				assert.NotNil(t, err, tc.description)
+			} else {
+				assert.Nil(t, err, tc.description)
+				tc.check(db)
+			}
+		})
+	}
+}
+
+func TestClientsForm_Delete(t *testing.T) {
+	testCases := []struct {
+		name        string
+		form        forms.Clients
+		check       func(db *gorm.DB)
+		wantErr     bool
+		description string
+	}{
+		{
+			name: "Delete third client",
+			form: forms.Clients{
+				ID: 3,
+			},
+			check: func(db *gorm.DB) {
+				var clients domain.Clients
+				err := db.First(&clients, 3).Error
+				assert.NotNil(t, err)
+				assert.Equal(t, err.Error(), gorm.ErrRecordNotFound.Error(), "This row not be found")
+			},
+			wantErr:     false,
+			description: "Delete third client from data storage and check it",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tests.PrepareTestDatabase()
+
+			err := tc.form.Delete(db)
+			if tc.wantErr {
+				assert.NotNil(t, err, tc.description)
+			} else {
+				assert.Nil(t, err, tc.description)
+				tc.check(db)
 			}
 		})
 	}
